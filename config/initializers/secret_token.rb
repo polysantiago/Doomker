4911f9a354f564315b9d0a9a67399d8aed206e48
Pablo Santiago <pablo.santiago@gmail.com>
2012-02-27 09:59:15 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doomker::Application.config.secret_token = '3f8d249069f8dcc293f330898be593f68dabdc00e25a30e73e61e6c098fb755ad4e9340d23ccaddc18989776996076309c7ae81a5703a3855588dc64a0621e49'
