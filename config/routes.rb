7af34a0e1dc8dda3b5e45d067e0be30775dd5e3e
Pablo Santiago <pablo.santiago@gmail.com>
2012-02-27 16:15:11 -0300

    sasas

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Doomker::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # resources :link
 
   # Sample resource route with options:
   #   resources :products do
@@ -58,5 +58,6 @@ Doomker::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
+  
 end
